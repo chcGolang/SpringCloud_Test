7b92caa9f0cb1d1ed6236e17ab6619c274433e87
chc_asus <869672271@qq.com>
2019-01-09 14:44:36 +0800

    product加入统一配置

--- client/order/service/src/main/java/com/chc/order_service/controller/EnvController.java
+++ client/order/service/src/main/java/com/chc/order_service/controller/EnvController.java
@@ -1,6 +1,8 @@
 package com.chc.order_service.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/env")
 @RefreshScope
 public class EnvController {
-/*
     @Value("${env}")
     private String env;
 
+    /**
+     * 查看加载统一配置类型
+     * @return
+     */
     @GetMapping("/print")
     public String print() {
         return env;
-    }*/
+    }
 }
